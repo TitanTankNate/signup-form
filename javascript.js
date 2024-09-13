0cf13f7db051a98c981da51cd0d733e45eb68a2e
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-09-13 15:17:02 -0400

    Add form field validation
    + Added the following validation
    + + First Name RegEx
    + + Last Name RegEx
    + + Phone Number RegEx
    
    signup-form_v0.0.3

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello world.");
+// DOM Query Selectors
+let submitButton = document.querySelector("button");    // Submit button
+let form = document.querySelector("form");              // Form
+let formInputs = document.querySelectorAll("input");    // Form fields
+
+submitButton.addEventListener("click", () => {
+    console.log("Clicked submit button.");
+
+    // DEBUG: If a user clicks the submit button, take all of the form data
+    // and print it to the console.
+    formInputs.forEach((input) => {
+        console.log(input.id + ": " + input.value);
+    });
+
+    form.reset();
+});
